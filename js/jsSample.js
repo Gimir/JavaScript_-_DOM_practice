17c479a5cfaa2c77b15e454f61a8ab2f807b09ad
Abbas Amaev <mukhammad.amaev@mail.ru>
2018-04-15 14:55:37 +0300

    Some animatino

--- js/jsSample.js
+++ js/jsSample.js
@@ -1,10 +1,24 @@
 var demo = document.getElementById("demo");
-var h = document.getElementsByTagName("h1");
 demo.innerHTML = "Hello World";
-demo.onclick = function() {
+function change() {
   if (demo.style.color == "green") demo.style.color = "red"
   else demo.style.color = "green"
 }
-for (let i = 0; i < h.length; i++) {
-  h[i].innerHTML = "BOOOMM!!!"
+var box = document.getElementById("box");
+function changeColor() {
+  if (box.className == "noact") box.className = "act"
+  else box.className = "noact"
+}
+setInterval(changeColor, 1000)
+
+var animate = document.getElementById('animate');
+var pos = 0;
+var id = setInterval(frame, 5);
+function frame(){
+  if (pos == 350) clearInterval(id)
+  else {
+    pos++;
+    animate.style.top = pos + 'px';
+    animate.style.left = pos + 'px';
+  }
 }
